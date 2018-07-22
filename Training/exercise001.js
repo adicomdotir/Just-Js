930dc7f11d4cc6bc310dac67e5e6cc150cc5a242
adicomdotir <adicom.ir@gmail.com>
2018-07-22 08:54:42 +0430

    this keyword example

--- Training/exercise001.js
+++ Training/exercise001.js
@@ -1,12 +1,12 @@
-let promise = new Promise((resolve, reject) => {
-	// resolve('done!');
-	reject('error');
-});
+// let promise = new Promise((resolve, reject) => {
+// 	// resolve('done!');
+// 	reject('error');
+// });
 
-promise.then(
-	resolve => alert(resolve),
-	// error => console.log(error)
-).catch(alert);
+// promise.then(
+// 	resolve => alert(resolve),
+// 	// error => console.log(error)
+// ).catch(alert);
 
 // What is this!!
 // javascript
@@ -18,8 +18,14 @@ promise.then(
 })('adicom');
 */
 
-(function(func){
-	func('adicom');
-})(function(name) {
-	console.log('hello ' + name);
-});
+// (function(func){
+// 	func('adicom');
+// })(function(name) {
+// 	console.log('hello ' + name);
+// });
+
+function indentify() {
+	return this.name.toUpperCase();
+}
+let me = {name: 'adicom'};
+console.log(indentify.call());
