b5f6575f3f79e1891b0f3ee97c82565066bf3d79
Adicom.ir <adicom.ir@gmail.com>
2018-08-24 09:16:28 +0430

    add other sample

--- nodejs-sample/app.js
+++ nodejs-sample/app.js
@@ -1,18 +1,10 @@
-const http = require('http');
-const date = require('./myfirstmodule');
-var url = require('url');
+var http = require('http');
+var fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + date.myDateTime());
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+    res.write(data);
+    res.end();
+  });
+}).listen(3001);
