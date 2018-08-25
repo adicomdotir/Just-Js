9cce1bce36a78667d33ce103da24d1682fdf800d
Adicom.ir <adicom.ir@gmail.com>
2018-08-25 21:02:20 +0430

    modify exercise001 file

--- Training/exercise001.js
+++ Training/exercise001.js
@@ -1,84 +1,11 @@
-// let promise = new Promise((resolve, reject) => {
-// 	// resolve('done!');
-// 	reject('error');
-// });
-
-// promise.then(
-// 	resolve => alert(resolve),
-// 	// error => console.log(error)
-// ).catch(alert);
-
-// What is this!!
-// javascript
-// Must be train js
-
-/*
-(function(name){
-	console.log('hello ' + name);
-})('adicom');
-*/
-
-// (function(func){
-// 	func('adicom');
-// })(function(name) {
-// 	console.log('hello ' + name);
-// });
-
-function indentify() {
-	return this.name.toUpperCase();
-}
-let me = {
-	name: 'adicom'
+// const f = (m) => () => console.log(m);
+const f = function (m) {
+  return function () {
+    return console.log(m);
+  };
 };
-console.log(indentify.call());
-
-function foo() {
-	console.log( this.a );
-	console.log(this);
+// const f2 = (f3) => f3();
+const f2 = function (f3) {
+	f3();
 }
-var obj = {
-	a: 2,
-	foo: foo
-};
-obj.foo(); // 2
-
-class Vehicle {
-    engines = 1
-    ignition() {
-        output("Turning on my engine.");
-    }
-    drive() {
-        ignition();
-        output("Steering and moving forward!")
-    }
-}
-class Car inherits Vehicle {
-    wheels = 4
-    drive() {
-        inherited: drive()
-        output("Rolling on all ", wheels, " wheels!")
-    }
-}
-class SpeedBoat inherits Vehicle {
-    engines = 2
-    ignition() {
-        output("Turning on my ", engines, " engines.")
-    }
-    pilot() {
-        inherited: drive()
-        output("Speeding through the water with ease!")
-    }
-}
-
-// Bad idea
-var x = new Boolean(false);
-if (x) {
-  alert('hi');  // Shows 'hi'.
-}
-// Good idea
-var x = Boolean(0);
-if (x) {
-  alert('hi');  // This will never be alerted.
-}
-typeof Boolean(0) == 'boolean'; // <--- good
-typeof new Boolean(0) == 'object'; // <--- bad
+f2(f('Test'));
