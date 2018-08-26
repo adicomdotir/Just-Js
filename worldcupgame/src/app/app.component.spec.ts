2f052beed299340bb3a4fb3ee67c3920c82e37d5
Adicom.ir <adicom.ir@gmail.com>
2018-08-26 21:06:44 +0430

    add angular game and delete old file

--- worldcupgame/src/app/app.component.spec.ts
+++ worldcupgame/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Sample'`, async(() => {
+  it(`should have as title 'worldcupgame'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Sample');
+    expect(app.title).toEqual('worldcupgame');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Sample!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to worldcupgame!');
   }));
 });
