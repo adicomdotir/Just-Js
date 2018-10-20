a58f1e37ca7504097241d6fee3e4e4e5df31debf
adicomdotir <adicom.ir@gmail.com>
2018-10-20 11:02:50 +0330

    add rxjs sample for angular

---
+++ Angular/Other/main.ts
@@ -0,0 +1,21 @@
+const subject: Subject<string> = new Subject<string>();
+subject.subscribe(value => console.log(value));
+subject.next('one');
+// output one
+
+const observable: Observable<string> = subject.asObservable();
+observable.subscribe(value => console.log(value));
+
+const subject1 = new BehaviorSubject<string>('zero');
+subject1.subscribe(value => console.log(value));
+subject1.next('one');
+// output zero, one
+
+const subject2 = new ReplaySubject<string>(2);
+subject1.next('one');
+subject1.next('two');
+subject1.next('three');
+subject1.next('four');
+subject2.subscribe(value => console.log(value));
+subject1.next('five');
+// output three, four, five
