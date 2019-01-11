234c01319e2ff7329c130e02a448ed2761669cdb
adicomdotir <adicom.ir@gmail.com>
2019-01-11 09:25:37 +0330

    bunnyEars question

--- CodingBat/Recursion-1/sample.js
+++ CodingBat/Recursion-1/sample.js
@@ -1,6 +1,9 @@
-function factorial (n) {
-    if (n === 0) {
-        return 1;
-    }
-     return n * factorial(n-1)
- }
+function factorial(n) {
+    if (n === 0) return 1;
+    return n * factorial(n - 1)
+}
+
+function bunnyEars(bunnies) {
+    if (bunnies <= 0) return 0;
+    return 2 + bunnyEarsRecursive(bunnies - 1);;
+}
