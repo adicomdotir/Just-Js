c8169bce17e00a1ad8c05782cc57bd5288c8a879
adicomdotir <adicom.ir@gmail.com>
2019-01-12 10:13:05 +0330

    bunnyEars2 question

--- CodingBat/Recursion-1/sample.js
+++ CodingBat/Recursion-1/sample.js
@@ -12,3 +12,9 @@ function fibonacci(n) {
     if (n < 2) return n;
     return fibonacci(n - 1) + fibonacci(n - 2)
 }
+
+function bunnyEars2(n) {
+    if (n == 0) return 0;
+    if (n % 2 == 0) return 3 + bunnyEars2(n - 1);
+    return 2 + bunnyEars2(n - 1);
+}
