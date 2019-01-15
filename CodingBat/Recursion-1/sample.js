caab99e77a87ef1ff9f47c2b1a44bbcaa10731ee
adicomdotir <adicom.ir@gmail.com>
2019-01-15 08:51:28 +0330

    countX question

--- CodingBat/Recursion-1/sample.js
+++ CodingBat/Recursion-1/sample.js
@@ -50,3 +50,10 @@ function powerN(base, n){
     if (n == 1) return base;
     return base * powerN(base, n - 1);
 }
+
+function countX(str){
+    if (str.length <= 0) return 0;
+    if (str.charAt(0) == 'x') 
+        return 1 + countX(str.substring(1));
+    return countX(str.substring(1));
+}
