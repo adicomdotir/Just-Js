52a6bae5eea983b154a7bf3ff2af1cb5725e6585
adicomdotir <adicom.ir@gmail.com>
2019-01-16 08:52:24 +0330

    noX question

--- CodingBat/Recursion-1/sample.js
+++ CodingBat/Recursion-1/sample.js
@@ -72,3 +72,10 @@ function changePi(str){
 	}
 	return str.charAt(0)+ changePi(str.substring(1));
 }
+
+function noX(str){
+  	if (str.length <= 0) return '';
+  	if (str.charAt(0) == 'x')
+    	return noX(str.substring(1));
+  	return str.charAt(0) + noX(str.substring(1));
+}
