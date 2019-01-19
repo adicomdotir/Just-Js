dde9e0da553ffc67bb43580a177105e01fcba845
adicomdotir <adicom.ir@gmail.com>
2019-01-19 08:53:20 +0330

    endX question

--- CodingBat/Recursion-1/sample.js
+++ CodingBat/Recursion-1/sample.js
@@ -108,3 +108,10 @@ function pairStar(str){
     }
     return str.charAt(0) + pairStar(str.substring(1));
 }
+
+function endX(str){
+    if (str.length <= 1) return str;
+    if (str.charAt(0) == 'x')
+        return endX(str.substring(1)) + str.charAt(0);
+    return str.charAt(0) + endX(str.substring(1))
+}
