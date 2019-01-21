7cd32f3a35b7e04c22cc6a6240e9779e6078269a
Adicom.ir <adicom.ir@gmail.com>
2019-01-21 13:55:20 +0330

    countHi2 question

--- CodingBat/Recursion-1/sample.js
+++ CodingBat/Recursion-1/sample.js
@@ -145,3 +145,19 @@ function stringClean(str){
   }
   return str.charAt(0) + stringClean(str.substring(1));
 }
+
+function countHi2 (str){
+  if (str.length <= 1){
+    return 0;
+  }
+
+  if (str.substring(0, 3) == "xhi"){
+    return countHi2(str.substring(3))
+  }
+
+  if (str.substring(0, 2) == "hi"){
+     return 1 + countHi2(str.substring(2))
+  }
+
+   return countHi2(str.substring(1))
+}
