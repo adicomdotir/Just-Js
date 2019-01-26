ad24980704e0eeac751d6d90c0fb676c9273fa2b
adicomdotir <adicom.ir@gmail.com>
2019-01-26 09:19:10 +0330

    nestParen question

--- CodingBat/Recursion-1/sample.js
+++ CodingBat/Recursion-1/sample.js
@@ -161,3 +161,14 @@ function starBit (str) {
 
   return starBit(str.substring(1, str.length - 1));
 }
+
+function nestParen(str){
+  if(str.length == 0)
+        return true;
+
+    if(str.charAt(0) == '(' && str.charAt(str.length-1) == ')'){
+        return nestParen(str.substring(1, str.length-1));
+    }
+
+    return false;
+}
