b61da96a539f1e483524c6c1b8550e675fae0d7a
adicomdotir <adicom.ir@gmail.com>
2019-01-27 12:26:18 +0330

    strDist question

--- CodingBat/Recursion-1/sample.js
+++ CodingBat/Recursion-1/sample.js
@@ -205,3 +205,27 @@ function strCopies(str, sub, n) {
     }
     return strCopies(str.substring(1), sub, n);
 }
+
+function strDist(str, sub) {
+    if (str.length <= sub.length) {
+        if (str == sub) {
+            return str.length;
+        } else {
+            return 0;
+        }
+    }
+
+    if (str.substring(0, sub.length) == sub && str.substring(str.length - sub.length) == sub) {
+        return str.length;
+    }
+
+    if (str.substring(0, sub.length) == sub) {
+        return strDist(str.substring(0, str.length - 1), sub);
+    }
+
+    if (str.substring(str.length - sub.length) == sub) {
+        return strDist(str.substring(1), sub);
+    }
+
+    return strDist(str.substring(1, str.length - 1), sub)
+}
