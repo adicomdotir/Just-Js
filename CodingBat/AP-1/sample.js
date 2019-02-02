8d4a0305d18ef29abdd35aa34016168b0b72b72f
adicomdotir <adicom.ir@gmail.com>
2019-02-02 11:19:23 +0330

    wordsFront question

--- CodingBat/AP-1/sample.js
+++ CodingBat/AP-1/sample.js
@@ -60,3 +60,23 @@ function wordsCount(words, len) {
     }
     return count;
 }
+
+/*
+Given an array of strings, return a new array containing the first N strings. 
+N will be in the range 1..length.
+
+Examples
+
+wordsFront(['a', 'b', 'c', 'd'], 1) → a
+wordsFront(['a', 'b', 'c', 'd'], 2) → a,b
+wordsFront(['a', 'b', 'c', 'd'], 3) → a,b,c
+*/
+
+function wordsFront(words, n){
+    let newWords = [];
+    for (let i = 0; i < n; i++) {
+        const element = words[i];
+        newWords.push(element);
+    }
+    return newWords;
+}
