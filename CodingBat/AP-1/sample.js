ea87821feef17d826e3a768047b744236698cc1c
adicomdotir <adicom.ir@gmail.com>
2019-02-02 11:35:52 +0330

    hasOne question

--- CodingBat/AP-1/sample.js
+++ CodingBat/AP-1/sample.js
@@ -102,3 +102,23 @@ function wordsWithoutList(words, len){
     }
     return newWords;
 }
+
+/*
+Given a positive int n, return true if it contains a 1 digit. 
+Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+
+Examples
+
+hasOne(10) → true
+hasOne(22) → false
+hasOne(220) → false
+*/
+
+function hasOne(n) {
+    while(n > 0) {
+        let d = n % 10;
+        if (d === 1) return true;
+        n = Math.floor(n / 10);
+    }
+    return false;
+}
