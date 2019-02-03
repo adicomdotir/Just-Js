34ebdcd26a02a700a4a1c0a39142b1d9648158eb
adicomdotir <adicom.ir@gmail.com>
2019-02-03 09:01:23 +0330

    dividesSelf question

--- CodingBat/AP-1/sample.js
+++ CodingBat/AP-1/sample.js
@@ -122,3 +122,26 @@ function hasOne(n) {
     }
     return false;
 }
+
+/*
+We'll say that a positive int divides itself if every digit in the number divides into the number evenly. 
+So for example 128 divides itself since 1, 2, and 8 all divide into 128 evenly. 
+We'll say that 0 does not divide into anything evenly, so no number with a 0 digit divides itself. 
+Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+
+Examples
+
+dividesSelf(128) → true
+dividesSelf(12) → true
+dividesSelf(120) → false
+*/
+
+function dividesSelf(n) {
+    let temp = n;
+    while (temp > 0) {
+        let d = temp % 10;
+        if (n % d != 0) return false;
+        temp = Math.floor(temp / 10);
+    }
+    return true;
+}
