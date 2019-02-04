7f393f57391da1becc5856f18ad61debb06e59b4
adicomdotir <adicom.ir@gmail.com>
2019-02-04 08:56:06 +0330

    matchUp question

--- CodingBat/AP-1/sample.js
+++ CodingBat/AP-1/sample.js
@@ -187,9 +187,32 @@ function copyEndy(nums, count) {
     let newArr = [];
     for (let i = 0; i < nums.length, newArr.length < count; i++) {
         const element = nums[i];
-        if (element % 2 == 0) {
+        if (element < 10 && element >= 0) {
+            newArr.push(element);
+        } else if (element < 100 && element >= 90) {
             newArr.push(element);
         }
     }
     return newArr;
+}
+
+/*
+Given arrays nums1 and nums2 of the same length, for every element in nums1, 
+consider the corresponding element in nums2 (at the same index). 
+Return the count of the number of times that the two elements differ by 2 or less, but are not equal.
+
+Examples
+
+matchUp([1, 2, 3], [2, 3, 10]) → 0
+matchUp([1, 2, 3], [2, 3, 5]) → 0
+matchUp([1, 2, 3], [2, 3, 3]) → 0
+*/
+
+function matchUp(a, b) {
+    let count = 0;
+    for (let j = 0; j < a.length; j++) {
+        if (Math.abs(a[j] - b[j]) == 2 && Math.abs(a[j] - b[j]) == 2) {
+            count++;
+        }
+    }
 }
