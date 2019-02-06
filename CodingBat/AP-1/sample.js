3cb84169e58069e9daeb6d9be218e6c53bf12b89
adicomdotir <adicom.ir@gmail.com>
2019-02-06 08:59:03 +0330

    scoresSpecial question

--- CodingBat/AP-1/sample.js
+++ CodingBat/AP-1/sample.js
@@ -268,3 +268,35 @@ function wordsWithout(words, target){
     }
     return newArr;
 }
+
+/*
+Given two arrays, A and B, of non-negative int scores. A "special" score is one which is a multiple of 10, 
+such as 40 or 90. Return the sum of largest special score in A and the largest special score in B. 
+To practice decomposition, write a separate helper method which finds the largest special score in an array. Write your helper method after your scoresSpecial() method in the JavaBat text area.
+
+Examples
+
+scoresSpecial([12, 10, 4], [2, 20, 30]) → 40
+scoresSpecial([20, 10, 4], [2, 20, 10]) → 40
+scoresSpecial([12, 11, 4], [2, 20, 31]) → 20
+*/
+
+function scoresSpecial(a, b){
+    let maxA = 0, maxB = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] % 10 == 0) {
+            if (a[i] > maxA) {
+                maxA = a[i];
+            }
+        }
+    }
+
+    for (let i = 0; i < b.length; i++) {
+        if (b[i] % 10 == 0) {
+            if (b[i] > maxB) {
+                maxB = b[i];
+            }
+        }
+    }
+    return maxA + maxB;
+}
