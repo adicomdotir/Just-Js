d6699154f8a351aaa6c5d3d3fa0b0ae4c322b2bd
adicomdotir <adicom.ir@gmail.com>
2019-02-14 09:11:02 +0330

    userCompare question

--- CodingBat/AP-1/sample.js
+++ CodingBat/AP-1/sample.js
@@ -364,6 +364,42 @@ bigHeights([5, 3, 6, 7, 2], 0, 1) → 0
 bigHeights([5, 3, 6, 7, 2], 0, 4) → 1
 */
 
-function bigHeights(heights, start, end){
-    
+function bigHeights(heights, start, end) {
+    var count = 0;
+    for (var i = start; i < end; i++) {
+        if (Math.abs(heights[i] - heights[i + 1]) >= 5) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+/*
+We have data for two users, A and B, each with a String name and an int id. 
+The goal is to order the users such as for sorting. 
+Return -1 if A comes before B, 1 if A comes after B, and 0 if they are the same. 
+Order first by the string names, and then by the id numbers if the names are the same.
+Note: with Strings str1.compareTo(str2) returns an int value which is negative/0/positive 
+to indicate how str1 is ordered to str2 (the value is not limited to -1/0/1).
+(On the AP, there would be two User objects, but here the code simply takes the two strings and two 
+ints directly. The code logic is the same.)
+
+Examples
+
+userCompare('bb', 1, 'zz', 2) → -1
+userCompare('bb', 1, 'aa', 2) → 1
+userCompare('bb', 1, 'bb', 1) → 0
+*/
+
+function userCompare(aName, aId, bName, bId) {
+    if (aName < bName) {
+        return -1;
+    } else if (aName > bName) {
+        return 1;
+    } else if (aId < bId) {
+        return -1;
+    } else if (aId > bId) {
+        return 1;
+    }
+    return 0;
 }
