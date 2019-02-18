6297a4bdaa7f91b12cf5df43c3003af4c085a98e
adicomdotir <adicom.ir@gmail.com>
2019-02-18 11:10:02 +0330

    dateFashion

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -12,10 +12,38 @@ cigarParty(70, true) → true
 */
 
 function cigarParty(cigars, isWeekend) {
-	if (isWeekend == true && cigars >= 40) {
-		return true;
-	} else if (cigars >= 40 && cigars <= 60) {
-		return true;
-	}
-	return false;
+    if (isWeekend == true && cigars >= 40) {
+        return true;
+    } else if (cigars >= 40 && cigars <= 60) {
+        return true;
+    }
+    return false;
+}
+
+/*
+You and your date are trying to get a table at a restaurant. 
+The parameter "you" is the stylishness of your clothes, in the range 0..10, 
+and "date" is the stylishness of your date's clothes. 
+The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. 
+If either of you is very stylish, 8 or more, then the result is 2 (yes). 
+With the exception that if either of you has style of 2 or less, then the result is 0 (no). 
+'Otherwise the result is 1 (maybe).
+
+Examples
+
+dateFashion(5, 10) → 2
+dateFashion(5, 2) → 0
+dateFashion(5, 5) → 1
+*/
+
+function dateFashion(you, date) {
+    if (you <= 2 || date <= 2) {
+        return 0;
+    }
+
+    if (you >= 8 || date >= 8) {
+        return 2;
+    }
+
+    return 1;
 }
--- CodingBat/readme.md
+++ CodingBat/readme.md
@@ -5,4 +5,4 @@ JS Challenge for training
 
 - [x] Recursion-1 31/31
 - [x] AP-1 21/21
-- [ ] Logic-1 1/X
+- [ ] Logic-1 2/29
