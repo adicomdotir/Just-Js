3978f07c044298ca885c79a555915fe1f65f7c11
adicomdotir <adicom.ir@gmail.com>
2019-03-15 08:44:04 +0330

    love6 question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -139,4 +139,20 @@ function alarmClock(day, vacation){
 	return "off";
 }
 
+/*
+The number 6 is a truly great number. Given two int values, a and b, 
+return true if either one is 6. Or if their sum or difference is 6. 
+Note: the function Math.abs(num) computes the absolute value of a number.
 
+Examples
+
+love6(6, 4) → true
+love6(4, 5) → false
+love6(1, 5) → true
+*/
+
+function love6(a, b){
+    if (a == 6 || b == 6) return true;
+    if (Math.abs(a - b) == 6) return true;
+    return false;
+}
