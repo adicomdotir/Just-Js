e53b9c47251033998004d43f968f9d3c2db55951
adicomdotir <adicom.ir@gmail.com>
2019-03-31 14:47:15 +0430

    old35 question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -208,3 +208,21 @@ function more20(n) {
     if (n % 20 == 1 || n % 20 == 2) return true;
     return false;
 }
+
+/*
+Return true if the given non-negative number is a multiple of 3 or 5, but not both. 
+Use the % "mod" operator.
+
+Examples
+
+old35(3) → true
+old35(10) → true
+old35(15) → false
+*/
+
+function old35(n){
+    if (n % 3 == 0 && n % 5 == 0) return false;
+    if (n % 3 == 0 || n % 5 == 0) return true;
+    return false;
+}
+
