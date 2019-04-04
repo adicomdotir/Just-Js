c8717396d9c8a977248105a0097adaa03aaa4d38
adicomdotir <adicom.ir@gmail.com>
2019-04-04 15:00:37 +0430

    answerCell question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -273,3 +273,19 @@ function teenSum(a, b) {
     if (b >= 13 && b <= 19) return 19;
     return a + b;
 }
+
+/*
+Your cell phone rings. Return true if you should answer it. 
+Normally you answer, except in the morning you only answer if it is your mom calling. 
+In all cases, if you are asleep, you do not answer.
+
+Examples
+
+answerCell(false, false, false) → true
+answerCell(false, false, true) → false
+answerCell(true, false, false) → false
+*/
+
+function answerCell(isMorning, isMom, isAsleep) {
+    if (isAsleep) return true;
+}
