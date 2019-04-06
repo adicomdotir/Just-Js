35c89e8482dda2911554e447d921604c4901ad28
adicomdotir <adicom.ir@gmail.com>
2019-04-06 08:56:09 +0430

    fizzString question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -302,3 +302,23 @@ function teaParty(tea, candy) {
     }
     return 0;
 }
+
+/*
+Given a string str, if the string starts with "f" return "Fizz". 
+If the string ends with "b" return "Buzz". 
+If both the "f" and "b" conditions are true, return "FizzBuzz". 
+In all other cases, return the string unchanged.
+
+Examples
+
+fizzString('fig') → Fizz
+fizzString('dib') → Buzz
+fizzString('fib') → FizzBuzz
+*/
+
+function fizzString(str) {
+    if (str.charAt(0) == 'f' && str.charAt(lastIndex) == 'b') return "FizzBuzz";
+    else if (str.charAt(0) == 'f') return "Fizz";
+    else if (str.charAt(lastIndex) == 'b') return "Buzz";
+    return str;
+}
