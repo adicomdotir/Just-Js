bd9b28513fdf887db227b5c9abc6ddde12fd0eea
adicomdotir <adicom.ir@gmail.com>
2019-04-07 08:58:50 +0430

    fizzString2 question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -317,9 +317,30 @@ fizzString('fib') → FizzBuzz
 */
 
 function fizzString(str) {
-	let lastIndex = str.length - 1;
+    let lastIndex = str.length - 1;
     if (str.charAt(0) == 'f' && str.charAt(lastIndex) == 'b') return "FizzBuzz";
     else if (str.charAt(0) == 'f') return "Fizz";
     else if (str.charAt(lastIndex) == 'b') return "Buzz";
     return str;
+}
+
+/*
+Given an int n, return the string form of the number followed by "!". 
+So the int 6 yields "6!". Except if the number is divisible by 3 use "Fizz" instead of the number, 
+and if the number is divisible by 5 use "Buzz", and if divisible by both 3 and 5, use "FizzBuzz". 
+Note: the % "mod" operator computes the remainder after division, so 23 % 10 yields 3. 
+What will the remainder be when one number divides evenly into another?
+
+Examples
+
+fizzString2(1) → 1!
+fizzString2(2) → 2!
+fizzString2(3) → Fizz!
+*/
+
+function fizzString2(n) {
+    if (n % 3 == 0 && n % 5 == 0) return "FizzBuzz!";
+    else if (n % 3 == 0) return "Fizz!";
+    else if (n % 5 == 0) return "Buzz!";
+    return n + '!';
 }
