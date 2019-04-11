df13aa65d944d12a08b97a050ab874d431283e1c
adicomdotir <adicom.ir@gmail.com>
2019-04-11 08:56:20 +0430

    endUp question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -412,3 +412,21 @@ function lastDigit(a, b, c) {
     return a % 10 == b % 10 || b % 10 == c % 10 || a % 10 == c;
 }
 
+/*
+Given a string, return a new string where the last 3 chars are now in upper case.
+If the string has less than 3 chars, uppercase whatever is there.
+Note that str.toUpperCase() returns the uppercase version of a string.
+
+Examples
+
+endUp('Hello') → HeLLO
+endUp('hi there') → hi thERE
+endUp('hi') → HI
+*/
+
+function endUp(str) {
+    if (str.length < 3) return str.toUpperCase();
+    return str.substring(0, str.length - 3) + str.substring(str.length - 3).toUpperCase();
+}
+
+
