0977855bb2f11fd5640f867a7c0f05327c8b3666
adicomdotir <adicom.ir@gmail.com>
2019-04-11 09:01:58 +0430

    stringsTimes question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -448,3 +448,21 @@ function everyNth(str, n) {
     }
     return newStr;
 }
+
+/*
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+Examples
+
+stringTimes('Hi', 2) → HiHi
+stringTimes('Hi', 3) → HiHiHi
+stringTimes('Hi', 1) → Hi
+*/
+
+function stringTimes(str, n){
+    let newStr = '';
+    for (let i = 0; i < n; i += 1) {
+        newStr += str;
+    }
+    return newStr;
+}
