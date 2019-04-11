fdc6065e8c5cc10df554bc78bcca9e98104fb88d
adicomdotir <adicom.ir@gmail.com>
2019-04-11 08:59:43 +0430

    everyNth question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -429,4 +429,22 @@ function endUp(str) {
     return str.substring(0, str.length - 3) + str.substring(str.length - 3).toUpperCase();
 }
 
+/*
+Given a non-empty string and an int N, return the string made starting with char 0,
+and then every Nth char of the string.
+So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
 
+Examples
+
+everyNth('Miracle', 2) → Mrce
+everyNth('abcdefg', 2) → aceg
+everyNth('abcdefg', 3) → adg
+*/
+
+function everyNth(str, n) {
+    let newStr = '';
+    for (let i = 0; i < str.length; i += n) {
+        newStr += str.charAt(i);
+    }
+    return newStr;
+}
