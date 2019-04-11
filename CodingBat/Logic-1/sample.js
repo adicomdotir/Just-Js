6e8e40e6d01969160395ef56ec620b6a7aee74ef
adicomdotir <adicom.ir@gmail.com>
2019-04-11 09:06:38 +0430

    frontTimes question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -466,3 +466,23 @@ function stringTimes(str, n){
     }
     return newStr;
 }
+
+/*
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
+or whatever is there if the string is less than length 3. Return n copies of the front.
+
+Examples
+
+frontTimes('Chocolate', 2) → ChoCho
+frontTimes('Chocolate', 3) → ChoChoCho
+frontTimes('Abc', 3) → AbcAbcAbc
+*/
+
+function frontTimes(str, n){
+    let newStr = '';
+    for (let i = 0; i < n; i += 1) {
+        if (str.length < 3) newStr += str;
+        else newStr += str.substring(0, 3);
+    }
+    return newStr;
+}
