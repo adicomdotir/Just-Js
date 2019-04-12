fa653a5e7350671fcad34e3450bbff3927cbb630
adicomdotir <adicom.ir@gmail.com>
2019-04-12 16:01:15 +0430

    maxMod5 question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -438,3 +438,18 @@ function withoutDoubles(die1, die2, noDoubles) {
     }
     return die1 + die2;
 }
+
+function maxMod5(a, b) {
+    if (a == b) {
+        return 0;
+    }
+
+    if (a % 5 == b % 5) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+    return a > b ? a : b;
+}
