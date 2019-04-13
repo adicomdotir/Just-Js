2f2bef43de11be05712ed22ab7299fb185f270ad
adicomdotir <adicom.ir@gmail.com>
2019-04-13 09:26:25 +0430

    sumLimit question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -523,3 +523,29 @@ function shareDigit(a, b) {
     return false;
 }
 
+/*
+Given 2 non-negative ints, a and b, return their sum,
+so long as the sum has the same number of digits as a.
+If the sum has more digits than a, just return a without b.
+(Note: one way to compute the number of digits of a non-negative int n is to convert
+it to a string with String.valueOf(n) and then check the length of the string.)
+
+Examples
+
+sumLimit(2, 3) → 5
+sumLimit(8, 3) → 8
+sumLimit(8, 1) → 9
+*/
+
+function sumLimit(a, b){
+    const aLen = Math.abs(a).toString().length;
+    const bLen = Math.abs(b).toString().length;
+    const res = a + b;
+    const resLen = Math.abs(res).toString().length;
+
+    if (resLen == aLen) {
+        return res;
+    } else if (resLen >= aLen) {
+        return a;
+    }
+}
