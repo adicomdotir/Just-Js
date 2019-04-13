7073cee9a4794c33812f4eb4197bc25aa275a8d3
adicomdotir <adicom.ir@gmail.com>
2019-04-13 09:25:58 +0430

    shareDigit question

--- CodingBat/Logic-1/sample.js
+++ CodingBat/Logic-1/sample.js
@@ -491,3 +491,35 @@ function greenTicket(a, b, c){
     if (a == b || a == c || b == c) return 10;
     return 0;
 }
+
+function blueTicket() {
+    // Dont have question
+}
+
+/*
+Given two ints, each in the range 10..99,
+return true if there is a digit that appears in both numbers, such as the 2 in 12 and 23.
+(Note: division, e.g. n/10, gives the left digit while the % "mod" n%10 gives the right digit.)
+
+Examples
+
+shareDigit(12, 23) → true
+shareDigit(12, 43) → false
+shareDigit(12, 44) → false
+*/
+
+function shareDigit(a, b) {
+    const aFirst = Math.floor(a / 10);
+    const aLast = a % 10;
+
+    if (aFirst == Math.floor(b / 10) || aFirst == b % 10) {
+        return true;
+    }
+
+    if (aLast == Math.floor(b / 10) || aLast == b % 10) {
+        return true;
+    }
+
+    return false;
+}
+
