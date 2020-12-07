55c282a10c0ee837108f04de8112d112b7780401
Yashar Panahi <adicom.ir@gmail.com>
2020-12-07 11:38:23 +0330

    Update and rename graph to graph.ts

--- TypeScript/graph.ts
+++ TypeScript/graph.ts
@@ -42,3 +42,42 @@ class Custom {
         return connectedComponents;
     }
 }
+
+class Custom {
+    mark = [];
+    adj: number[][] = [];
+    n: number;
+    start: number;
+    target: number;
+
+
+    constructor(edge: number[], n: number, s, t) {
+        this.n = n;
+        this.start = s;
+        this.target = t;
+        for (let i = 0; i < n; i++) {
+            this.adj[i + 1] = [];
+            this.mark[i + 1] = false;
+        }
+        for (let i = 0; i < edge.length; i += 2) {
+            this.adj[edge[i]].push(edge[i + 1]);
+            this.adj[edge[i + 1]].push(edge[i]);
+        }
+    }
+
+    dfs(v: number) {
+        this.mark[v] = true;
+        for (let i = 0; i < this.adj[v].length; i++) {
+            const elm = this.adj[v][i];
+            if (this.mark[elm] === false) {
+                this.dfs(elm);
+            }
+        }
+    }
+
+    solve(): number[] {
+        this.dfs(this.start);
+
+        return this.mark[this.target];
+    }
+}
