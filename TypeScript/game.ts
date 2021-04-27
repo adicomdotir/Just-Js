f58f3d54591ac923e6a7f7e7352ba00f1d2889a9
Yashar Panahi <adicom.ir@gmail.com>
2021-04-27 16:20:46 +0430

    Update game.ts

--- TypeScript/game.ts
+++ TypeScript/game.ts
@@ -1,65 +1,97 @@
 class Game {
     lastChampion: Array<string> = [];
+    NAMES = ['Bayern München',
+        'Manchester City',
+        'Real Madrid',
+        'Inter Milan',
+        'Barcelona',
+        'Paris Saint-Germain',
+        'Manchester United',
+        'Chelsea FC',
+        'Atlético Madrid',
+        'Ajax Amsterdam',
+        'Juventus',
+        'Liverpool FC',
+        'FC Porto',
+        'Atalanta',
+        'RasenBallsport Leipzig',
+        'Sevilla',
+        'River Plate',
+        'Borussia Dortmund',
+        'SSC Napoli',
+        'Eintracht Frankfurt',
+        'Wolfsburg',
+        'Slavia Prague',
+        'AC Milan',
+        'Arsenal',
+        'Villarreal',
+        'Tottenham Hotspur',
+        'Leicester City',
+        'Flamengo',
+        'Lazio',
+        'Lyon',
+        'Sporting',
+        'Benfica',
+        'Shakhtar Donetsk',
+        'Roma',
+        'Lille',
+        'Rangers',
+        'Boca Juniors',
+        'West Ham United',
+        'FK Red Star Belgrade',
+        'Al Ahly'];
+    LEAGUE_SIZE = 10;
+    teamIndex = [];
+    relegation = [];
 
     constructor() {
-        this.justiceLeague();
+        const names = this.NAMES;
+        const leagueSize = this.LEAGUE_SIZE;
+        for (let i = 0; i < leagueSize; i++) {
+            let idx = Math.floor(Math.random() * names.length);
+            while (this.teamIndex.filter(x => x === idx).length > 0) {
+                idx = Math.floor(Math.random() * names.length);
+            }
+            this.teamIndex.push(idx);
+            console.log(`${names[idx]} is Ranking ${idx + 1}`);
+        }
+        this.justiceLeague(leagueSize, this.teamIndex, names);
+        this.changeTeam();
+    }
+
+    changeTeam() {
+        const x = this.relegation[0];
+        const y = this.relegation[1];
+        let idx = Math.floor(Math.random() * this.NAMES.length);
+        let idy = Math.floor(Math.random() * this.NAMES.length);
+        while (this.teamIndex.filter(item => item === idx || item === idy).length > 0 || idx === idy || idx === x || idy === y || idx === y || idy === x) {
+            idx = Math.floor(Math.random() * this.NAMES.length);
+            idy = Math.floor(Math.random() * this.NAMES.length);
+        }
+        const deleteId = [];
+        this.teamIndex.forEach((v, i) => {
+            if (v === x) {
+                deleteId.push(i);
+            }
+            if (v === y) {
+                deleteId.push(i);
+            }
+        });
+        this.teamIndex.splice(deleteId[0], 1);
+        this.teamIndex.splice(deleteId[1], 1);
+        this.teamIndex.push(idx);
+        this.teamIndex.push(idy);
+        console.log(`Promotion 1 => ${this.NAMES[idx]} & Promotion 2 => ${this.NAMES[idy]}`);
+        console.log(`Relegation 1 => ${this.NAMES[x]} & Relegation 2 => ${this.NAMES[y]}`);
+        this.relegation = [];
     }
 
     nextYear() {
-        this.justiceLeague();
+        this.justiceLeague(this.LEAGUE_SIZE, this.teamIndex, this.NAMES);
+        this.changeTeam();
     }
 
-    private justiceLeague() {
-        const names = ['Bayern München',
-            'Manchester City',
-            'Real Madrid',
-            'Inter Milan',
-            'Barcelona',
-            'Paris Saint-Germain',
-            'Manchester United',
-            'Chelsea FC',
-            'Atlético Madrid',
-            'Ajax Amsterdam',
-            'Juventus',
-            'Liverpool FC',
-            'FC Porto',
-            'Atalanta',
-            'RasenBallsport Leipzig',
-            'Sevilla',
-            'River Plate',
-            'Borussia Dortmund',
-            'SSC Napoli',
-            'Eintracht Frankfurt',
-            'Wolfsburg',
-            'Slavia Prague',
-            'AC Milan',
-            'Arsenal',
-            'Villarreal',
-            'Tottenham Hotspur',
-            'Leicester City',
-            'Flamengo',
-            'Lazio',
-            'Lyon',
-            'Sporting',
-            'Benfica',
-            'Shakhtar Donetsk',
-            'Roma',
-            'Lille',
-            'Rangers',
-            'Boca Juniors',
-            'West Ham United',
-            'FK Red Star Belgrade',
-            'Al Ahly'];
-        const teamIndex = [];
-        const leagueSize = 6;
-        for (let i = 0; i < leagueSize; i++) {
-            let idx = Math.floor(Math.random() * names.length);
-            while (teamIndex.filter(x => x === idx).length > 0) {
-                idx = Math.floor(Math.random() * names.length);
-            }
-            teamIndex.push(idx);
-            console.log(`${names[idx]} is Ranking ${idx + 1}`);
-        }
+    private justiceLeague(leagueSize, teamIndex, names) {
         const fixtures: { h, hg, ag, a }[] = this.generateFixture(leagueSize);
         for (let i = 0; i < fixtures.length; i++) {
             if (i % (leagueSize / 2) === 0) {
@@ -128,12 +160,16 @@ class Game {
             }
         }
         console.log(' ');
-        console.log('Name'.toString().padStart(22, ' ') + `\t\tG\t\tW\t\tD\t\tL\t\tF\t\tA\t\tD\t\tP`);
+        console.log('Name'.toString().padStart(22, ' ') + `\t\tR\t\tG\t\tW\t\tD\t\tL\t\tF\t\tA\t\tD\t\tP`);
         tables.sort((a, b) => b.pts - a.pts || (b.gf - b.ga) - (a.gf - a.ga));
         for (const table of tables) {
-            console.log(`${names[teamIndex[table.id]].toString().padStart(22, ' ')}\t\t${table.g}\t\t${table.w}\t\t${table.d}\t\t${table.l}\t\t${table.gf}\t\t${table.ga}\t\t${table.gf - table.ga}\t\t${table.pts}`);
+            console.log(`${names[teamIndex[table.id]].toString().padStart(22, ' ')}\t\t${teamIndex[table.id]}\t\t${table.g}\t\t${table.w}\t\t${table.d}\t\t${table.l}\t\t${table.gf}\t\t${table.ga}\t\t${table.gf - table.ga}\t\t${table.pts}`);
         }
         this.lastChampion.push(names[teamIndex[tables[0].id]]);
+        console.log(this.lastChampion);
+
+        this.relegation.push(teamIndex[tables[tables.length - 1].id]);
+        this.relegation.push(teamIndex[tables[tables.length - 2].id]);
     }
 
     private generateFixture(count) {
